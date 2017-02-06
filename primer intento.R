d9acd46663eb2155810dd24d4b177fdd60e3c590
jonx18 <jonamar10@hotmail.com>
2017-02-06 10:09:32 -0300

    limpieza

--- primer intento.R
+++
@@ -1,4 +0,0 @@
-r <- POST("https://en.wikipedia.org/w/index.php?title=Special:Export", 
-            +           body = "&pages=Pope&offset=2001-12-05T12:04:10Z&limit=100&action=submit")
-stop_for_status(r)
-content(r, "parsed", "application/xml")
